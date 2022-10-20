41e9dca99d8d863ddaf1146e8c11941ddea54d0e
Chrisx19 <107272321+Chrisx19@users.noreply.github.com>
2022-10-20 00:05:09 -0700

    Update mainpp.cpp

--- mainpp.cpp
+++ mainpp.cpp
@@ -1,23 +1,18 @@
 #include <mainpp.h>
 #include <ros.h>
 #include <std_msgs/Float64.h>
-#include <std_msgs/String.h>
 #include <sensor_msgs/Joy.h>
 
 extern TIM_HandleTypeDef htim1;
 
 ros::NodeHandle nh;
 
-
-std_msgs::String str_msg;
 std_msgs::Float64 val;
-ros::Publisher chatter("chatter", &str_msg);
 ros::Publisher duty("duty_cycle", &val);
 
 void joy_cb(const sensor_msgs::Joy& vel_msg);
 
 ros::Subscriber<sensor_msgs::Joy> joy_sub("joy", &joy_cb);
-
 
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart){
   nh.getHardware()->flush();
@@ -27,12 +22,9 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
   nh.getHardware()->reset_rbuf();
 }
 
-
-
 void setup(void)
 {
   nh.initNode();
-  nh.advertise(chatter);
   nh.advertise(duty);
   nh.subscribe(joy_sub);
 
@@ -41,9 +33,6 @@ void setup(void)
 
 void loop(void)
 {
-
-  str_msg.data = "Hello from STM32F767ZI";
-  chatter.publish(&str_msg);
   duty.publish(&val);
 
   nh.spinOnce();
@@ -53,12 +42,11 @@ void loop(void)
 
 void joy_cb(const sensor_msgs::Joy& vel_msg)
 {
-
 	float weight = vel_msg.axes[1];
-
 	float duty_c = weight * 100;
-    HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
-    __HAL_TIM_SET_COMPARE(&htim1,TIM_CHANNEL_1, duty_c*400);
+	
+    	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+    	__HAL_TIM_SET_COMPARE(&htim1,TIM_CHANNEL_1, duty_c*400);
 
   	val.data = duty_c;
 	duty.publish(&val);
