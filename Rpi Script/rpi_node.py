e3a309bce286287a2224968b6676a8ff454130f3
Chrisx19 <107272321+Chrisx19@users.noreply.github.com>
2022-10-21 01:33:29 -0700

    Create rpi_node.py

---
+++ Rpi Script/rpi_node.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import rospy
+from sensor_msgs.msg import Joy
+from geometry_msgs.msg import Twist
+
+class Rpi(object):
+    def __init__(self):
+        rospy.loginfo("Starting Raspberry Pi node...")
+
+        self.cmd_vel_pub = rospy.Publisher("/cmd_vel_ugv", Twist, queue_size = 10)
+        self.joy_sub = rospy.Subscriber("/joy", Joy, self.joy_cb, queue_size=10)
+
+    def joy_cb(self, joy_msg):
+        self.vel = Twist()
+
+        joy_val_drive = joy_msg.axes[1]
+        joy_val_turn = joy_msg.axes[3]
+
+        duty = joy_val_drive * 100
+
+        self.vel.linear.x = duty
+        self.vel.angular.x = joy_val_turn
+
+        self.cmd_vel_pub.publish(self.vel)
+
+if __name__ == "__main__":
+    rospy.init_node("Rpi_node", anonymous = False)
+    rpi_n = Rpi()
+    rospy.spin()
